89dfcbbdbcb0b516ae37ca4ebe6dbe476369f8b5
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-05-06 02:37:42 +0300

    good start

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import style from './Feedback.module.css';
 
 export class FeedBack extends Component {
   state = {
@@ -7,9 +8,34 @@ export class FeedBack extends Component {
     bad: 0,
   };
 
-  handleBtn = () => {};
+  handleBtn = event => {
+    console.log(event.target.textContent);
+  };
 
   countTotalFeedback = () => {};
 
   countPositiveFeedbackPercentage = () => {};
+
+  render() {
+    return (
+      <div className={style.container}>
+        <h2>Please leave feedback</h2>
+        <button className={style.btn} onClick={this.handleBtn}>
+          Good
+        </button>
+        <button className={style.btn} onClick={this.handleBtn}>
+          Neutral
+        </button>
+        <button className={style.btn} onClick={this.handleBtn}>
+          Bad
+        </button>
+        <h2>Statistics</h2>
+        <ul>
+          <li>Good: {}</li>
+          <li>Neutral: {}</li>
+          <li>Bad: {}</li>
+        </ul>
+      </div>
+    );
+  }
 }
